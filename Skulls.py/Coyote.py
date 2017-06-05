6f3f1dce5b9bac2e44f7be1777f7da0b3110f2f0
Nigel-J <nijaffe@packer.edu>
2017-06-05 10:13:36 -0400

    Add files via upload

---
+++ Skulls.py/Coyote.py
@@ -0,0 +1,7 @@
+#Coyote - Canis latrans - Cytochrome C oxidase subunit 1
+
+c='MFINRWLFSTNHKDIGTLYLLFGAWAGMVGTALSLLIRAELGQPGTLLGDDQIYNVVVTAHAFVMIFFMVMPIMIGGFGNWLVPLMIGA'
+
+#https://www.ncbi.nlm.nih.gov/nuccore/91702689
+
+
---
+++ Skulls.py/Opossum.py
@@ -0,0 +1,6 @@
+#Opossum - Didelphis virginiania - Cytochrome C oxidase subunit 1
+
+d='MFINRWLFSTNHKDIGTLYLLFGAWAGMVGTALSLLIRAELGQPGTLIGDDQIYNVIVTAHAFIMIFFMVMPIMIGGFGNWLVPLMIGA'
+
+#https://www.ncbi.nlm.nih.gov/nuccore/452251
+
